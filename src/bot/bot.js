3a4cb0576ba084e2b8c7350d73c41d8d341c2ad8
Kagu-chan <k.boese@kagu-chan.de>
2019-02-25 21:22:09 +0100

    moved files

--- src/bot/bot.js
+++ src/bot/bot.js
@@ -4,33 +4,33 @@ const _ = require('lodash');
 const bot = {};
 
 _.assign(bot, {
-  getClient: require('./get/client').bind(undefined, bot),
-  getGuilds: require('./get/guilds').bind(undefined, bot),
-  getChannels: require('./get/channels').bind(undefined, bot),
-  getBotChannels: require('./get/bot-channels').bind(undefined, bot),
-  getApiReponse: require('./get/api-response').bind(undefined, bot),
+  getClient: require('./lib/get/client').bind(undefined, bot),
+  getGuilds: require('./lib/get/guilds').bind(undefined, bot),
+  getChannels: require('./lib/get/channels').bind(undefined, bot),
+  getBotChannels: require('./lib/get/bot-channels').bind(undefined, bot),
+  getApiReponse: require('./lib/get/api-response').bind(undefined, bot),
 
-  handleApiRequest: require('./handle/api-request').bind(undefined, bot),
+  handleApiRequest: require('./lib/handle/api-request').bind(undefined, bot),
 
-  parseArguments: require('./parse/arguments').bind(undefined, bot),
+  parseArguments: require('./lib/parse/arguments').bind(undefined, bot),
 
-  doConnect: require('./do/connect').bind(undefined, bot),
-  doSend: require('./do/send').bind(undefined, bot),
+  doConnect: require('./lib/do/connect').bind(undefined, bot),
+  doSend: require('./lib/do/send').bind(undefined, bot),
 
-  eventReady: require('./event/ready').bind(undefined, bot),
+  eventReady: require('./lib/event/ready').bind(undefined, bot),
 
-  eventChannelCreate: require('./event/channel/create').bind(undefined, bot),
-  eventChannelDelete: require('./event/channel/delete').bind(undefined, bot),
-  eventChannelUpdate: require('./event/channel/update').bind(undefined, bot),
+  eventChannelCreate: require('./lib/event/channel/create').bind(undefined, bot),
+  eventChannelDelete: require('./lib/event/channel/delete').bind(undefined, bot),
+  eventChannelUpdate: require('./lib/event/channel/update').bind(undefined, bot),
 
-  eventGuildCreate: require('./event/guild/create').bind(undefined, bot),
-  eventGuildDelete: require('./event/guild/delete').bind(undefined, bot),
+  eventGuildCreate: require('./lib/event/guild/create').bind(undefined, bot),
+  eventGuildDelete: require('./lib/event/guild/delete').bind(undefined, bot),
 
-  eventMessage: require('./event/message').bind(undefined, bot),
+  eventMessage: require('./lib/event/message').bind(undefined, bot),
 
-  registerEvents: require('./register/events').bind(undefined, bot),
-  registerCommand: require('./register/command').bind(undefined, bot),
-  registerCommands: require('./register/commands').bind(undefined, bot),
+  registerEvents: require('./lib/register/events').bind(undefined, bot),
+  registerCommand: require('./lib/register/command').bind(undefined, bot),
+  registerCommands: require('./lib/register/commands').bind(undefined, bot),
 });
 
 _.assign(bot, {
