bde97f5838a87a5f90d1bab6adabc4dfd97d5b2a
Kagu-chan <k.boese@kagu-chan.de>
2019-02-26 22:22:22 +0100

    let app error handler use logging

--- src/bot/core/handle-application-error.js
+++ src/bot/core/handle-application-error.js
@@ -1,13 +1,13 @@
 module.exports = (bot) => {
   process.on('uncaughtException', (err) => {
-    console.log('[ERROR]', 'Caught exception:\n', `${err.name}\n${err.message}\n${err.stack}`); // eslint-disable-line no-console
+    bot.logging.logError('Caught exception:\n', `${err.name}\n${err.message}\n${err.stack}`);
 
     const { client } = bot;
 
     if (client && client.status === 0) {
       client.fetchUser(process.env.BOT_OWNER)
         .then(user => user.send(`\`\`\`\n${err.name}\n${err.message}\n${err.stack}\`\`\``))
-        .catch(console.log.bind(undefined, 'ERROR')); // eslint-disable-line no-console
+        .catch(bot.logging.logError);
     }
   });
 };
