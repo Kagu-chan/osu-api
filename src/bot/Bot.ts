62ed2e2986ab64110488b6edbaf31cd6af917f48
Kagu-chan <k.boese@kagu-chan.de>
2019-03-03 19:18:58 +0100

    remove unused bot event handler

--- src/bot/Bot.ts
+++ src/bot/Bot.ts
@@ -1,4 +1,3 @@
-import BotEventHandler from './BotEventHandler';
 import ClientEventHandler from './ClientEventHandler';
 import ProcessEventHandler from './ProcessEventHandler';
 import Client from './Client';
@@ -12,11 +11,6 @@ export default class Bot {
    * @var {Client} client The discord client
    */
   public readonly client: Client;
-
-  /**
-   * @var {BotEventHandler} botEventHandler
-   */
-  private readonly botEventHandler: BotEventHandler;
 
   /**
    * @var {ClientEventHandler} clientEventHandler
@@ -40,15 +34,23 @@ export default class Bot {
       discordOwnerId: configuration.discordOwnerId
     });
 
-    this.botEventHandler = new BotEventHandler(this);
     this.clientEventHandler = new ClientEventHandler(this);
     this.processEventHandler = new ProcessEventHandler(this);
   }
 
   public initialize(): void {
-    this.botEventHandler.registerEvents();
     this.clientEventHandler.registerEvents();
     this.processEventHandler.registerEvents();
+  }
+
+  /**
+   * Login and trigger the bot processing
+   *
+   * @note This method may exit the process in case of connection issues
+   * @returns {Promise<void>}
+   */
+  public async run(): Promise<void> {
+    await this.client.login();
   }
 
   /**
@@ -62,41 +64,4 @@ export default class Bot {
       setTimeout(resolve, milliseconds);
     });
   }
-
-  /**
-   * Login and trigger the bot processing
-   *
-   * @note This method may exit the process in case of connection issues
-   * @returns {Promise<void>}
-   */
-  public async run(): Promise<void> {
-    await this.client.login();
-  }
-
-  // /**
-  //  * Event handler for login. Start internal processes
-  //  *
-  //  * @note This method may exit the process in case of unhandled exception
-  //  * @returns {Promise<void>}
-  //  */
-  // private async onClientLogin(sender: Client): Promise<void> {
-  //   try {
-  //     // @TODO
-  //     await sender.sendMessage(
-  //       [await sender.getOwner()],
-  //       ['Hallo', 'Welt']
-  //     );
-  //     this.logInfo('Logout...');
-  //     await sender.logout();
-  //     process.exit();
-  //   } catch (error) {
-  //     this.logError('Unhandled exception - exit process:', error);
-
-  //     if (sender.isOnline()) {
-  //       await sender.logout();
-  //     }
-
-  //     process.exit(1);
-  //   }
-  // }
 };
